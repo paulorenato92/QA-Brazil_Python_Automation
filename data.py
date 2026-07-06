11ce4cb117f56deaae2e08473c57d7a369b341ac
paulorenato92 <renato_723@hotmail.com>
2026-07-06 22:32:45 +0100

    Finalizando o projeto Sprint 7

---
+++ data.py
@@ -0,0 +1,10 @@
+#URL DO URBAN ROUTES
+URBAN_ROUTES_URL = 'https://cnt-7f3b4d19-a185-4927-946d-3cda8f7c0aba.containerhub.tripleten-services.com?lng=pt'
+
+# CONSTANTE DE USO NO TESTE
+ADDRESS_FROM = 'East 2nd Street, 601'
+ADDRESS_TO = '1300 1st St'
+PHONE_NUMBER = '+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Pare no bar de sucos'
---
+++ main.py
@@ -0,0 +1,43 @@
+import data
+import helpers as helpers
+
+class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
+            print("Conectado ao servidor Urban Routes")
+        else:
+            print("Não foi possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução.")
+# test_set_route, test_select_plan, test_fill_phone_number, test_fill_card, test_comment_for_driver, test_order_blanket_and_handkerchiefs, test_order_2_ice_creams e test_car_search_model_appears
+def test_set_route(self):
+    # Adicionar S8
+    print("função criada para definir a rota")
+    pass
+def test_select_plan(self):
+    # Adicionar S8
+    print("função criada para definir a rota")
+    pass
+def est_fill_phone_number(self):
+    # Adicionar S8
+    print("função criada para definir a rota")
+    pass
+def test_fill_card(self):
+    # Adicionar S8
+    print("função criada para definir a rota")
+    pass
+def test_comment_for_driver(self):
+    # Adicionar S8
+    print("função criada para definir a rota")
+    pass
+def test_order_blanket_and_handkerchiefs(self):
+    # Adicionar S8
+    print("função criada para definir a rota")
+    pass
+def test_order_2_ice_creams(self):
+    for _ in range(2):
+        print()
+    pass
+def test_car_search_model_appears(self):
+    # Adicionar S8
+    print("função criada para definir a rota")
+    pass
