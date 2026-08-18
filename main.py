5ec71d41d9267e192e5e9cc4901032f7f4f0f81b
paulorenato92 <renato_723@hotmail.com>
2026-08-18 22:03:55 +0100

    main.py

--- main.py
+++ main.py
@@ -9,77 +9,84 @@ from pages import UrbanRoutesPage
 
 class TestUrbanRoutes:
 
-    driver = None
-    page = None
+driver = None
+page = None
 
-    @classmethod
-    def setup_class(cls):
-        chrome_options = Options()
-        chrome_options.set_capability("goog:loggingPrefs", {'performance': 'ALL'})
-        
-        cls.driver = webdriver.Chrome(
-            service=Service(ChromeDriverManager().install()), 
-            options=chrome_options
-        )
-        cls.driver.implicitly_wait(10)
-        
-        cls.page = UrbanRoutesPage(cls.driver)
-        cls.driver.get(data.URBAN_ROUTES_URL)
-        
-        if helpers.is_url_reachable(data.URBAN_ROUTES_URL):
-            print("Conectado ao servidor Urban Routes")
-        else:
-            print("Não é possível conectar ao Urban Routes. Verifique se o servidor está ligado e ainda em execução.")
+@classmethod
+def setup_class(cls):
+chrome_options = Options()
+chrome_options.set_capability("goog:loggingPrefs", {'performance': 'ALL'})
+cls.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+cls.driver.implicitly_wait(10)
+cls.page = UrbanRoutesPage(cls.driver)
 
-    @classmethod
-    def teardown_class(cls):
-        if cls.driver:
-            cls.driver.quit()
+def setup_method(self):
+self.driver.get(data.URBAN_ROUTES_URL)
 
-    def test_set_route(self):
-        address_from = data.ADDRESS_FROM
-        address_to = data.ADDRESS_TO
-        self.page.set_route(address_from, address_to)
-        assert self.page.get_from() == address_from
-        assert self.page.get_to() == address_to
+def _prepare_route_and_plan(self):
+self.page.set_route(data.ADDRESS_FROM, data.ADDRESS_TO)
+self.page.select_supportive_plan()
 
-    def test_select_plan(self):
-        self.page.select_supportive_plan()
-        assert self.page.get_current_selected_plan() == "Comfort"
+def _prepare_phone(self):
+self._prepare_route_and_plan()
+self.page.set_phone(data.PHONE_NUMBER)
 
-    def test_fill_phone_number(self):
-        phone_number = data.PHONE_NUMBER
-        self.page.set_phone(phone_number)
-        assert self.page.get_phone() == phone_number
+def _prepare_card(self):
+self._prepare_phone()
+self.page.set_card(data.CARD_NUMBER, data.CARD_CODE)
 
-    def test_fill_card(self):
-        self.page.set_card(data.CARD_NUMBER, data.CARD_CODE)
-        assert self.page.get_current_payment_method() == "Cartão"
+def test_set_route(self):
+self.page.set_route(data.ADDRESS_FROM, data.ADDRESS_TO)
+assert self.page.get_from() == data.ADDRESS_FROM
+assert self.page.get_to() == data.ADDRESS_TO
 
-    def test_comment_for_driver(self):
-        message = data.MESSAGE_FOR_DRIVER
-        self.page.set_message_for_driver(message)
-        assert self.page.get_message_for_driver() == message
+def test_select_plan(self):
+self.page.set_route(data.ADDRESS_FROM, data.ADDRESS_TO)
+self.page.select_supportive_plan()
+assert self.page.get_current_selected_plan() == "Comfort"
 
-    def test_order_blanket_and_handkerchiefs(self):
-        self.page.click_blanket_and_handkerchiefs_option()
-        assert self.page.get_blanket_and_handkerchiefs_option_checked()
+def test_fill_phone_number(self):
+self._prepare_route_and_plan()
+self.page.set_phone(data.PHONE_NUMBER)
+assert self.page.get_phone() == data.PHONE_NUMBER
 
-    def test_order_2_ice_creams(self):
-        self.page.add_ice_cream(2)
-        assert self.page.get_amount_of_ice_cream() == 2
+def test_fill_card(self):
+self._prepare_phone()
+self.page.set_card(data.CARD_NUMBER, data.CARD_CODE)
+assert self.page.get_current_payment_method() == "Cartão"
 
-    def test_car_search_model_appears(self):
-        self.page.click_order_taxi_buton()
-        assert self.page.wait_order_taxi_popup()
+def test_comment_for_driver(self):
+self._prepare_route_and_plan()
+self.page.set_message_for_driver(data.MESSAGE_FOR_DRIVER)
+assert self.page.get_message_for_driver() == data.MESSAGE_FOR_DRIVER
 
-    def test_driver_info_appears(self):
-        self.page.wait_driver_info()
-        name, rating, image = self.page.get_driver_info()
-        assert name 
-        assert rating 
-        assert image
+def test_order_blanket_and_handkerchiefs(self):
+self._prepare_route_and_plan()
+self.page.click_blanket_and_handkerchiefs_option()
+assert self.page.get_blanket_and_handkerchiefs_option_checked()
 
-    @classmethod
-    def teardown_class(cls):
-        cls.driver.quit()
+def test_order_2_ice_creams(self):
+self._prepare_route_and_plan()
+self.page.add_ice_cream(2)
+assert self.page.get_amount_of_ice_cream() == 2
+
+def test_car_search_model_appears(self):
+self._prepare_card()
+self.page.set_message_for_driver(data.MESSAGE_FOR_DRIVER)
+self.page.click_order_taxi_buton()
+assert self.page.wait_order_taxi_popup()
+
+def test_driver_info_appears(self):
+self._prepare_card()
+self.page.set_message_for_driver(data.MESSAGE_FOR_DRIVER)
+self.page.click_order_taxi_buton()
+self.page.wait_driver_info()
+name, rating, image = self.page.get_driver_info()
+assert name
+assert rating
+assert image
+
+@classmethod
+def teardown_class(cls):
+if cls.driver:
+cls.driver.quit()
